7226ff0033dabff3acfedfcff031f608ea25cc47
Pipo <pipobtz@gmail.com>
2021-02-04 07:48:38 +0100

    Setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
+import numeral from 'numeral'
 import { removeExpense } from '../actions/expenses'
 
 
@@ -9,7 +11,11 @@ const ExpenseListItem = (props) => (
         <Link to={`/edit/${props.expense.id}`}>
             <h3>{props.expense.description}</h3>
         </Link>
-        <p>{props.expense.amount} - {props.expense.createdAt}</p>
+        <p>
+            {numeral(props.expense.amount / 100).format('$0,0.00')} 
+            - 
+            {moment(props.expense.createdAt).format('MMMM Do, YYYY')}
+        </p>
         <button onClick={() => {
             // console.log('Should remove', props.expense.id)
             props.dispatch(removeExpense(props.expense.id))
