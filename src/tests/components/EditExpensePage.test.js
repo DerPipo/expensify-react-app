f4009a6bf07adcca546a1507a6e52c305a5befbf
Pipo <pipobtz@gmail.com>
2021-02-08 15:39:03 +0100

    Added edit functionality

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -3,13 +3,13 @@ import { shallow } from 'enzyme'
 import { EditExpensePage } from '../../components/EditExpensePage'
 import expenses from '../fixtures/expenses'
 
-let editExpense, history, startRemoveExpense, wrapper
+let startEditExpense, history, startRemoveExpense, wrapper
 
 beforeEach(() => {
-    editExpense = jest.fn()
+    startEditExpense = jest.fn()
     startRemoveExpense = jest.fn(() => Promise.resolve())
     history = { push: jest.fn() }
-    wrapper = shallow(<EditExpensePage expense={expenses[0]} editExpense={editExpense} startRemoveExpense={startRemoveExpense} history={history} />)
+    wrapper = shallow(<EditExpensePage expense={expenses[0]} startEditExpense={startEditExpense} startRemoveExpense={startRemoveExpense} history={history} />)
 })
 
 test('should render EditExpensePage', () => {
@@ -18,8 +18,8 @@ test('should render EditExpensePage', () => {
 
 test('should handle editExpense', () => {
     wrapper.find('ExpenseForm').simulate('submit', expenses[0])
-    expect(editExpense).toHaveBeenCalledTimes(1)
-    expect(editExpense).toHaveBeenLastCalledWith(expenses[0].id, expenses[0])
+    expect(startEditExpense).toHaveBeenCalledTimes(1)
+    expect(startEditExpense).toHaveBeenLastCalledWith(expenses[0].id, expenses[0])
     expect(history.push).toHaveBeenCalledTimes(1)
     expect(history.push).toHaveBeenLastCalledWith('/')
 })
